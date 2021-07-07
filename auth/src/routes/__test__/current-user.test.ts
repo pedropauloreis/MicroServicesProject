e72663e20f83f85c7479eb2c4983836c9c6ad3c6
Pedro Paulo de Oliveira Reis <ppreis@gmail.com>
2021-07-07 10:04:02 +0200

    fixing simulated test fail

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -12,7 +12,7 @@ it('Respond with details about the current user', async () => {
         .get('/api/users/currentuser')
         .set('Cookie',cookie)
         .send()
-        .expect(400);
+        .expect(200);
 
     expect(response.body.currentUser.email).toEqual('email@teste.com');
     
